3c2b154af81d5ad1134fa03b02ba28d5e94efdd4
Jatani Ali <mrcortesyali@gmail.co>
2025-10-28 11:18:18 +0300

    dist issue

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,8 +1,22 @@
 import { defineConfig } from 'vite'
 import tailwindcss from '@tailwindcss/vite'
+import path from 'path';
 
 export default defineConfig({
   plugins: [
     tailwindcss(),
   ],
+  build: {
+    outDir: 'dist',
+    emptyOutDir: true,
+  },
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+    },
+  },
+  server: {
+    port: 5173,
+    strictPort: true,
+  },
 })
