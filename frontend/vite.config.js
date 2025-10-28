81b4c147d68f1c5a821d23509e0a0ab77f3ba764
Jatani Ali <mrcortesyali@gmail.co>
2025-10-28 11:26:36 +0300

    tailwind issue

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,11 +1,10 @@
 import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
-import path from 'path';
+import path from 'path'
 
 export default defineConfig({
-  plugins: [
-    tailwindcss(),
-  ],
+  plugins: [react(), tailwindcss()],
   build: {
     outDir: 'dist',
     emptyOutDir: true,
