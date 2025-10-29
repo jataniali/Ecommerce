b07f4629ced0c46bd4675e3287753a4f2688befb
Jatani Ali <mrcortesyali@gmail.co>
2025-10-29 13:07:03 +0300

    corse policy issue fixed

--- Backend/index.js
+++ Backend/index.js
@@ -21,6 +21,7 @@ app.use(express.json());
 // ✅ CORS setup
 const allowedOrigins = [
   'https://ecommerce-frontend-4gjt.onrender.com',
+  'https://ecommerce-admin-hnzh.onrender.com',
   'http://localhost:5173',
   'http://localhost:5174',
   'http://localhost:3000',
